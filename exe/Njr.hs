a3006a9fbcb0c06a10cf934768ce889ef592bf3c
Nishant <njr@cubic>
2026-02-26 15:17:53 +0530

    exe/Njr.hs: keyboard binding for bemenu-run

--- exe/Njr.hs
+++ exe/Njr.hs
@@ -23,6 +23,7 @@ main = wxyz $
     keyBindings = M.fromList
         [ ((modMask, xkb_key_q),      terminate)
         , ((modMask, xkb_key_t),      shell "alacritty")
+        , ((modMask, xkb_key_d),      shell "bemenu-run")
         , ((modMask, xkb_key_h),      hello)
         , ((modMask, xkb_key_tab),    next_toplevel)
 
